17229470a85b0c2ee93e57f143ab3c9906257c47
ajit15091997 <127679183+ajit15091997@users.noreply.github.com>
2025-08-19 04:46:01 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ require('dotenv').config();
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.static('frontend'));
+app.use(express.static('public'));
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('✅ MongoDB connected'))
